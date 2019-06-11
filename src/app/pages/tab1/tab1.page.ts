0e0eaed9af24beff57f94e0bce69c4a0e63da660
Oscar <oscar.carrasco1980@gmail.com>
2019-06-10 21:30:07 -0400

    Termino de la clase 127

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Lista } from 'src/app/models/lista.model';
 
 @Component({
   selector: 'app-tab1',
@@ -42,11 +43,17 @@ export class Tab1Page {
             if ( data.titulo.length === 0 ) {
               return;
             }
-            this.deseosService.crearLista( data.titulo );
+            const listaId = this.deseosService.crearLista( data.titulo );
+
+            this.router.navigateByUrl(`/tabs/tab1/agregar/${ listaId }`);
           }
         }
       ]
     });
     alert.present();
+  }
+
+  listaSeleccionada ( lista: Lista) {
+    this.router.navigateByUrl(`/tabs/tab1/agregar/${ lista.id }`);
   }
 }
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Lista } from '../models/lista.model';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +10,34 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
-
-    const lista1 = new Lista('Recolectar piedras del infinito');
-    const lista2 = new Lista('Heroes a desaparecer');
-
-    this.listas.push(lista1, lista2);
-    
+    this.cargarStorage();
   }
 
   crearLista( titulo: string) {
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+
+    return nuevaLista.id;
+  }
+
+  obtenerLista( id: string | number ) {
+    id = Number(id);
+
+    return this.listas.find( listaData => listaData.id === id );
+
+  }
+  
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage() {
+    if ( localStorage.getItem('data') ) {
+      this.listas = JSON.parse( localStorage.getItem('data') );
+    } else {
+      this.listas = [];
+    }
   }
   
 }
